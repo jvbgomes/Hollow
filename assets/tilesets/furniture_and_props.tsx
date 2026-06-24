644b1a1f849f89bdf2f0e9dae2250e27592d0f33
João Victor <jvbg16@gmail.com>
2026-06-23 23:16:18 -0300

    Add furniture_and_props tileset config

---
+++ assets/tilesets/furniture_and_props.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="furniture_and_props" tilewidth="16" tileheight="16" tilecount="6708" columns="86">
+ <image source="furniture_and_props.png" width="1376" height="1248"/>
+</tileset>
