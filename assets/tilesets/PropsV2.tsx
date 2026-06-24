e3fb28163dae3c37bd1f1a8424e91c7b65385ea1
João Victor <jvbg16@gmail.com>
2026-06-23 23:16:11 -0300

    Add PropsV2 tileset

---
+++ assets/tilesets/PropsV2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="PropsV2" tilewidth="16" tileheight="16" tilecount="900" columns="30">
+ <image source="PropsV2.png" width="480" height="480"/>
+</tileset>
