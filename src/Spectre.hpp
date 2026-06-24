a8966001f8d8c736b35bb05f675480104a59b40a
João Victor <jvbg16@gmail.com>
2026-06-24 05:23:03 -0300

    Add obstacle avoidance and stuck detection to Spectre

--- src/Spectre.hpp
+++ src/Spectre.hpp
@@ -7,4 +7,28 @@ public:
     Spectre(float x, float y);
 
     void update(float deltaTime, const Map& map, sf::Vector2f playerPosition) override;
+    void draw(sf::RenderTarget& target) override;
+
+    bool canSeePlayer(sf::Vector2f playerPos) const;
+
+private:
+    static constexpr float VIEW_RADIUS    = 120.f;
+    static constexpr float VIEW_ANGLE_DEG = 70.f;
+    static constexpr float CHASE_MEMORY   = 3.f;   // segundos perseguindo após perder de vista
+
+    sf::Vector2f m_facingDir  = {0.f, 1.f};
+    bool         m_chasing    = false;
+    float        m_chaseTimer = 0.f;
+
+    // steering de desvio de obstáculos
+    float        m_stuckTimer  = 0.f;
+    sf::Vector2f m_stuckPos    = {};
+    float        m_escapeTimer = 0.f;   // tempo restante de fuga ativa
+    float        m_escapeAngle = 0.f;   // direção de fuga (offset em rad)
+
+    sf::VertexArray m_fovCone;
+    void rebuildFOVCone();
+
+    // retorna dir rotacionada por `angle` rad; {} se colide
+    sf::Vector2f probeDir(sf::Vector2f dir, float angle, float dt, const Map& map);
 };
