675246c29d79e5e8c1f67c18ea6925325224352f
João Victor <jvbg16@gmail.com>
2026-06-30 01:54:12 -0300

    Update Spectre header

--- src/Spectre.hpp
+++ src/Spectre.hpp
@@ -16,18 +16,13 @@ public:
     void setWaypoints(const std::vector<sf::Vector2f>& wp);
 
 private:
-    static constexpr float VIEW_RADIUS    = 120.f;
-    static constexpr float VIEW_ANGLE_DEG = 70.f;
-    static constexpr float CHASE_MEMORY   = 3.f;
+    static constexpr float VIEW_RADIUS    = 150.f;
+    static constexpr float VIEW_ANGLE_DEG = 80.f;
+    static constexpr float CHASE_MEMORY   = 6.f;
 
     sf::Vector2f m_facingDir  = {0.f, 1.f};
     bool         m_chasing    = false;
     float        m_chaseTimer = 0.f;
-
-    float        m_stuckTimer  = 0.f;
-    sf::Vector2f m_stuckPos    = {};
-    float        m_escapeTimer = 0.f;
-    float        m_escapeAngle = 0.f;
 
     // Patrulha
     std::vector<sf::Vector2f> m_waypoints;
@@ -36,6 +31,4 @@ private:
 
     sf::VertexArray m_fovCone;
     void rebuildFOVCone();
-
-    sf::Vector2f probeDir(sf::Vector2f dir, float angle, float dt, const Map& map);
 };
